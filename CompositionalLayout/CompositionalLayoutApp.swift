dc7a29fd6ce7405a80cd832c16f7c1aef6eb5bb3
Rendi Wijiatmoko <rendi.os@brins.co.id>
2022-04-26 10:08:34 +0700

    Initial Commit

---
+++ CompositionalLayout/CompositionalLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompositionalLayoutApp.swift
+//  CompositionalLayout
+//
+//  Created by Rendi Wijiatmoko on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CompositionalLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CompositionalLayout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CompositionalLayout
+//
+//  Created by Rendi Wijiatmoko on 26/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
